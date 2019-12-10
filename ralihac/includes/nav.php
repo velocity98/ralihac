013f3a8857630e2c4faaef9e2627f0f341474ae6
velocity98 <55411831+velocity98@users.noreply.github.com>
2019-12-09 20:04:25 -0500

    init commit

---
+++ ralihac/includes/nav.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-info">
+  <div class="container">
+    <span class="navbar-brand" href="index.php">Ralihac</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <form class="form-inline my-2 my-lg-0">
+           <input class="form-control mr-sm-2" type="search" placeholder="Search">
+           <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+         </form>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav" >
+        <li class="nav-item dropdown">
+         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           <i class="fas fa-user"></i> Hello User
+         </a>
+         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+           <a class="dropdown-item" href="#">Action</a>
+           <a class="dropdown-item" href="#">Another action</a>
+           <div class="dropdown-divider"></div>
+           <a class="dropdown-item" href="#">Logout</a>
+         </div>
+       </li>
+      </ul>
+    </div>
+  </div>
+
+</nav>
---
+++ ralihac/index.php
@@ -0,0 +1,89 @@
+<?php
+require_once 'system/initialize.php';
+include 'includes/head.php';
+include 'includes/nav.php';
+ ?>
+ <div class="container">
+   <div class="row row-margin">
+     <div class="col-md-3">
+      <div class="card widget" style="margin-top: 3rem; margin-bottom: 1rem;">
+        <div class="card-header">
+          <h5>Welcome to Ralihac</h5>
+        </div>
+        <div class="card-body">
+          <span>Try Ralihac's Randomize &nbsp</span><a class="fas fa-info-circle text-info"></a>
+          <button class="btn btn-block border border-dark bg-info text-light" style="margin-top: .5rem">Randomize</button>
+        </div>
+      </div>
+     </div>
+
+     <div class='col-md-9'>
+       <legend>
+         Featured Hacks
+       </legend>
+       <hr />
+       <div class='row'>
+         <div class='col-md-4'>
+           <div class='card widget card-spacing'>
+             <img src='images/siteimages/background.jpeg' style='width: auto; height:10rem;'/>
+             <div class='card-header'>
+               <span>Test</span>
+             </div>
+             <div class='card-body'>
+
+             </div>
+             <div class='card-footer text-secondary'>
+               <a class='fas fa-thumbs-up'> 2</a>
+             </div>
+           </div>
+         </div>
+       </div>
+       <br />
+      <legend>
+        New Hacks
+      </legend>
+      <hr />
+      <div class='row'>
+        <div class='col-md-4'>
+          <div class='card widget card-spacing'>
+            <img src='images/siteimages/background.jpeg' style='width: auto; height:10rem;'/>
+            <div class='card-header'>
+              <span>Test</span>
+            </div>
+            <div class='card-body'>
+
+            </div>
+            <div class='card-footer text-secondary'>
+              <a class='fas fa-thumbs-up'> 2</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <legend>
+        Most Liked Hacks
+      </legend>
+      <hr />
+      <div class='row'>
+        <div class='col-md-4'>
+          <div class='card widget card-spacing'>
+            <img src='images/siteimages/background.jpeg' style='width: auto; height:10rem;'/>
+            <div class='card-header'>
+              <span>Test</span>
+            </div>
+            <div class='card-body'>
+
+            </div>
+            <div class='card-footer text-secondary'>
+              <a class='fas fa-thumbs-up'> 2</a>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
+
+   </div>
+ </div>
+<?php
+include 'includes/footer.php';
+ ?>
