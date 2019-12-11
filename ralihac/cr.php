7b02ec1ece1886faeb30dc695181b37317dcd70a
velocity98 <55411831+velocity98@users.noreply.github.com>
2019-12-10 21:40:05 -0500

    init commit
    More UI, add functionaility (PHP) for login/signup

---
+++ ralihac/cr.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'system/initialize.php';
+include 'includes/head.php';
+include 'includes/nav.php';
+?>
+<div class='container'>
+  <div class='row align-items-center'>
+    <div class='col-md-8 offset-md-2'>
+      <div class="d-flex justify-content-center h-100" style='margin-top: 5rem;'>
+        <div class='row'>
+          <div class='col-md-8 offset-md-2'>
+              <h2><b>Create your New Account</b></h2>
+          </div>
+          <div class='col-md-8 offset-md-2'>
+              <h5>Get your ideas out there</h5>
+          </div>
+
+          <div class='col-md-8 offset-md-2'>
+            <br />
+
+            <form method="post" action="cr.php" onsubmit="return user_validate()" name="vform">
+              <!-- change vForm -->
+              <div class="input-group form-group">
+                  <input type="text" class="form-control" placeholder="Username" aria-describedby="user_error" name="username" value="">
+                  <small id="user_error" class="form-text val_error text-danger w-100"></small>
+              </div>
+
+              <div class="input-group form-group">
+                <input type="password" class="form-control" placeholder="Password" name="password" value="">
+                <small id="password_error" class="form-text val_error text-danger w-100"></small>
+              </div>
+
+              <div class="input-group form-group">
+                <input type="password" class="form-control" placeholder="Confirm Password" name="confirmPassword" value="">
+                <small id="confirmPassword_error" class="form-text val_error text-danger w-100"></small>
+              </div>
+
+              <div class="input-group form-group">
+                <input type="email" class="form-control" placeholder="Email" name="email" value="">
+                <small id="email_error" class="form-text val_error text-danger w-100"></small>
+              </div>
+
+              <div class="form-group">
+                <input type="submit" value="Sign Up" class="btn btn-light text-primary btn-block border-primary" name"signUp">
+              </div>
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
